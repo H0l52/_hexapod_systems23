688ed395a9f002a21a32e419c9fee47d1eff862c
H0l52 <71470962+H0l52@users.noreply.github.com>
2023-07-09 12:05:18 +1000

    LoRa

--- walk/walk.ino
+++ walk/walk.ino
@@ -2,6 +2,9 @@
 #include "commandInterpreter.h"
 #include "mLoop.h"
 
+#include <SPI.h>
+#include <LoRa.h>
+
 const int stepsPerRevolution = 2038;
 
 commandInterpreter cI(stepsPerRevolution);
@@ -9,6 +12,16 @@ mLoop _mloop(&cI);
 
 void setup() {
   Serial.begin(9600);
+
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
   cI.addLeg(new int [12] {8, 10, 9, 11, 0, 0, 0, 0, 0, 0, 0, 0});
 }
 
@@ -19,5 +32,20 @@ void loop() {
     char r = Serial.read();
     String icString(r);
     _mloop.PushUpdate(icString);
+  }
+
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
   }
 }
