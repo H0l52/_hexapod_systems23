8b2d9d01d40211e5241e95014d00e1a641a9fc82
H0l52 <71470962+H0l52@users.noreply.github.com>
2023-09-25 20:03:43 +1000

    finalised comments

--- accelw/LegControl.cpp
+++ accelw/LegControl.cpp
@@ -2,17 +2,20 @@
 #include "LegControl.h"
 #include "MCP23008.h"
 
+
+// Initialize the leg control system
 LegControl::LegControl(int *i) {
   mlist = i;
-  //delay(1000);
   for (uint8_t e = 0; e < 6; e++) {
     uint8_t index = e * 6;
-
+    // Initialize each stepper motor
     AccelStepper xy(AccelStepper::FULL4WIRE, mlist[index], mlist[index+2], mlist[index+1], mlist[index+3], true);
 
+    // Setup each leg structure (Servos are implicit)
     Leg l;
     l.o_xy = xy;
 
+    // Add to legs list
     this->legs[this->leg_c++] = l;
   }
 }
@@ -21,37 +24,39 @@ LegControl::LegControl(int *i) {
 /// and returns the legcontrol class reference.
 void LegControl::setup(Servo *s, MCP23008* mp,MCP23008* mp2,MCP23008* mp3) {
 
+  // Connect the servos up to their respective lists
   for (int e = 0; e < 6; e++) {
     this->legs[e].o_z = s[e*2];
     this->legs[e].h_z = s[(e*2)+1];
   }
 
   this->currentPosition = vector2D(0, 0);
+
+  // Connect all the mcp pin expandors to the right legs in the code.
   this->legs[0].o_xy.mpcont = mp2;
   this->legs[1].o_xy.mpcont = mp3;
   this->legs[2].o_xy.mpcont = mp2;
   this->legs[3].o_xy.mpcont = mp3;
   this->legs[4].o_xy.mpcont = mp;
   this->legs[5].o_xy.mpcont = mp;
+
+  // Connect all the outputs 
   for (int i = 0; i < 6; i++) {
     this->legs[i].o_xy.enableOutputs();
-
-    
   }
-
-  
-  // this->legs[0].o_z.mpcont = mp;
-  // this->legs[0].o_z.enableOutputs();
-
-  // this->legs[0].h_z.mpcont = mp;
-  // this->legs[0].h_z.enableOutputs();
 }
 
 
 
+// Attempt to process an event from the list
 int LegControl::AttemptEventProc() {
+  // Check if there is an event to process, if not die
   if (this->currentEvent == NULL) return 0;
+
+  // If event, then we can first process the stepper motors
   Event::runMotors(this->legs);
+
+  // Now we can process the event.
   return this->currentEvent->Proc(this->legs, this->currentPosition);
 }
 
@@ -63,21 +68,26 @@ void LegControl::procStep() {
   if (this->currentEvent == NULL) return;
   /// If currentEvent should be take over by eventlist.next, set it, and drop the list size.
   if ((*this->currentEvent) <= (*(this->eventList[0]))) {
-    if (this->eventListSize == 0) this->currentEvent = NULL;
+    if (this->eventListSize == 0) this->currentEvent = NULL; //Is there actually another event.
     else {
+      // Replace the current with the next one.
       this->currentEvent = this->eventList[0];
+      // Move the memory of the entire list over by 1 in the stack, effectively removing the first element and reassigning the first element.
       memmove(&this->eventList[0], &this->eventList[1], this->eventListSize-- - 1);
     }
   }
 
   /// Double event send, assume error due to no override value set.
+  /// Same as above, but just override the memory.
   // while (this->eventListSize != 0 && ((*this->currentEvent) == (*(this->eventList[0])))) {
   //   memmove(&this->eventList[0], &this->eventList[1], this->eventListSize-- - 1);
   // }
 
-  /// Exit code hit
+  // Attempt to proc the event, take the status code
   int x = AttemptEventProc();
   if (x != 0) {
+    // If status is not 0, it died somehow, lets change over to a new event.
+    // Same techniques as above.
     Logging::Warning("Event Died");
     if (this->eventListSize == 0) this->currentEvent = NULL;
     else {
@@ -96,12 +106,13 @@ void LegControl::submitEvent(Event *ev, bool override) {
     return;
   }
 
-  /// If override is set, force the event in, and clear memory of the list.
+  /// If override is set, force the event in, and clear memory of the list, then return.
   if (override || (this->eventListSize == 0 && this->currentEvent == NULL)) {
     this->currentEvent = ev;
     memset(this->eventList, 0, this->MAX_EVENTLIST_SIZE);
     return;
   }
 
+  // Add the event to the end of the list
   this->eventList[this->eventListSize++] = ev;
 }
